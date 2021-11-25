aefb9215e0307ca5ef68983325d0385f2ddd6f17
Abhijeet Kulkarni <amkulk@udel.edu>
2021-11-25 15:48:16 -0500

    HW acc intel

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,27 @@
 # Author: amkulk@udel.edu 
 # Date:11/24/21
 # writing for rendering window
+
+# Edits
+# Date:11/25/2021
+#	Added working hardware acceleration for intel see #http://wiki.ros.org/docker/Tutorials/Hardware%20Acceleration
+
+#############
 FROM ubuntu:20.04
 # 4.10.3 latest at time of writing this script
 RUN apt-get update -y; apt-get upgrade -y; apt-get -y install ssh
 RUN apt install -y python3.8
 RUN apt install -y python3-pip
 RUN apt install -y patchelf
-RUN apt install -y libosmesa6-dev libgl1-mesa-glx libglfw3 
+RUN apt install -y libosmesa6-dev libgl1-mesa-glx libglfw3 libgl1-mesa-dri
+
+#http://wiki.ros.org/docker/Tutorials/Hardware%20Acceleration
+RUN rm -rf /var/lib/apt/lists/*
 #add pip dependencies here
 RUN pip install free-mujoco-py
+RUN pip install sympy
+RUN pip install casadi
+
 
 # importing mujoco_py once to build it
 RUN python3 -c 'import mujoco_py'
--- StartContainer.sh
+++ StartContainer.sh
@@ -1,12 +1,15 @@
 # Expose the X server on the host.
-sudo xhost +local:root # --rm: Make the container ephemeral (delete on exit).
+xhost +local:root # --rm: Make the container ephemeral (delete on exit).
 
 #http://wiki.ros.org/docker/Tutorials/GUI
 
 # -it: Interactive TTY.
 # --gpus all: Expose all GPUs to the container.
 # --rm removes the container after it is stopped
-docker run --rm -it -v $(pwd):/root/host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 --name mjdocker_container mjdocker bash
+#docker run --rm -it -v $(pwd):/root/host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 --name mjdocker_container mjdocker bash
+
+#http://wiki.ros.org/docker/Tutorials/Hardware%20Acceleration
+docker run --rm -it -v $(pwd):/root/host --volume=/tmp/.X11-unix:/tmp/.X11-unix --device=/dev/dri:/dev/dri --env="DISPLAY=$DISPLAY" --name mjdocker_container mjdocker bash
 
 #docker stop mjdocker_container
-sudo xhost -local:root
+xhost -local:root
