ef57ebd835c5bc46d5c12d16c250c46078c10ac1
Abhijeet Kulkarni <amkulk@udel.edu>
2021-11-25 01:36:56 -0500

    fixed gpg

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -22,8 +22,7 @@ apt-get update
 apt-get apt-get install ca-certificates curl gnupg lsb-release
 
 #Add Docker’s official GPG key:
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg 
-gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 #set up the stable repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
